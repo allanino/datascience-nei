302076d268eb4c4618a81d5f47a4184f53884f24
Allan Costa <allaninocencio@yahoo.com.br>
2014-10-25 22:53:58 -0700

    Half the way!
    Just remember to render those plots in Linux, as Windows don't do
    antialiasing.

--- plot2.R
+++ plot2.R
@@ -2,6 +2,9 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+# Keep only Baltimore results, having fips == "24510"
+NEI <- NEI[NEI$fips == "24510", ]
+
 # Sum the emissions by year
 emissions <- c(sum(NEI[NEI$year == 1999, "Emissions"]),
                sum(NEI[NEI$year == 2002, "Emissions"]),
@@ -10,7 +13,10 @@ emissions <- c(sum(NEI[NEI$year == 1999, "Emissions"]),
 years <- c("1999", "2002", "2005", "2008")
 
 # Create and save the plot
-png(filename = "plot1.png") 
-plot(years, emissions, type="l", main=expression("PM"[2.5]*" total emission"), 
-        xlab="Year", ylab=expression("PM"[2.5]*" emission (tons)"))
+png(filename = "plot2.png") 
+plot(years, emissions, 
+        type="l", 
+        main=expression("PM"[2.5]*" emission in Baltimore City, Maryland"), 
+        xlab="Year", 
+        ylab=expression("PM"[2.5]*" emission (tons)"))
 dev.off()
---
+++ plot3.R
@@ -0,0 +1,27 @@
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Keep only Baltimore results
+NEI <- NEI[NEI$fips == "24510", ]
+
+# Factor type and year, as they were originally character
+NEI$type <- factor(NEI$type)
+NEI$year <- factor(NEI$year)
+
+# Get sample data to speed up testing
+# sNEI <- NEI[sample(nrow(NEI), 10000), ]
+
+# Import ggplot2 package. Install it if it's not already instaled
+if(!require(ggplot2)){install.packages("ggplot2")}
+
+# Create and save the plot
+png(filename = "plot3.png") 
+p <-  ggplot(NEI, aes(x = year, y = Emissions)) +
+      stat_summary(fun.y = sum, geom="line", aes(group =1)) +
+      facet_grid(type ~ ., scales = "free") + 
+      xlab("Year") + 
+      ylab(expression("PM"[2.5]*" emission (tons)")) +
+      ggtitle(expression("PM"[2.5]*" emission by source type in Baltimore City, Maryland"))
+print(p)
+dev.off()
